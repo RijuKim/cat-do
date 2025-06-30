08472cd2143ecac758e9fdb7914ab949efe408b3
RijooKim <rijoo.kim@kt.com>
2025-06-30 22:36:30 +0900

    Fix 배포 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/cat-do',
-  assetPrefix: '/cat-do',
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
