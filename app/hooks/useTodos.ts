6abea1213ede827457154de0050ee5d07514c2a6
RijooKim <rijoo.kim@kt.com>
2025-07-22 21:08:27 +0900

    Fix 빌드 에러 수정

--- app/hooks/useTodos.ts
+++ app/hooks/useTodos.ts
@@ -27,7 +27,6 @@ export default function useTodos(
   const [editText, setEditText] = useState('');
 
   const selectedKey = date.toLocaleDateString('sv-SE');
-  const displayDate = `${date.getFullYear()}년 ${(date.getMonth() + 1).toString().padStart(2, '0')}월 ${date.getDate().toString().padStart(2, '0')}일`;
 
   // 날짜가 바뀌면 완료 조언 초기화
   useEffect(() => {
